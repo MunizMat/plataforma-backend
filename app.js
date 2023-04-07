00dc5873ab7f7a2f759eaa4bc80553238ef9dc9a
Matheus Muniz <103901768+MunizMat@users.noreply.github.com>
2023-04-07 15:18:50 -0300

    Alterações

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+require('body-parser');
 const homeRoutes = require('./src/routes/homeRoutes');
 const userRoutes = require('./src/routes/userRoutes');
 
 function App(){
     this.app = express();
+
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({extended: true}));
 
     // Rotas
     this.app.use('/', homeRoutes);
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
  
 const userController = {
+
     index: async (req, res) => {
         try {
             let users = await User.findAll();
@@ -11,15 +12,20 @@ const userController = {
             res.send('Não foi possível encontrar os usuários');
         }
     },
+
     store: async (req, res) => {
-        try {
-            await User.create(req.body);
-            res.send('O usuário foi salvo na base de dados!');
-        } catch (error) {
-            console.log(error);
-            res.send('Não foi possível salvar o usuário na base de dados');
-        }
+        console.log('Recebi dados do formulário');
+        console.log(req.body);
+        res.send('OK');
+        // try {
+        //     await User.create(req.body);
+        //     res.send('O usuário foi salvo na base de dados!');
+        // } catch (error) {
+        //     console.log(error);
+        //     res.send('Não foi possível salvar o usuário na base de dados');
+        // }
     },
+    
     delete: async (req, res) => {
         try {
             const user = await User.findOne({where: { id: req.params.id }});
@@ -29,6 +35,7 @@ const userController = {
             res.send('Não foi possível apagar o usuário na base de dados');
         }
     },
+
     update: (req, res) => {
         res.send('Update de users');
     },
