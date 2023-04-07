ce2ad8b541135e7ae10d6010377888b0a8b0df82
Matheus Muniz <103901768+MunizMat@users.noreply.github.com>
2023-04-07 18:09:28 -0300

    Configurando o express pra receber requisições do front-end

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express = require('express');
-require('body-parser');
+const cors =  require('cors');
 const homeRoutes = require('./src/routes/homeRoutes');
 const userRoutes = require('./src/routes/userRoutes');
 
 function App(){
     this.app = express();
 
+    this.app.use(cors({
+        origin: 'http://127.0.0.1:5173'
+    }))
     this.app.use(express.json());
     this.app.use(express.urlencoded({extended: true}));
 
